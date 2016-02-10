bffb573423816ab809fea1c51ca9d926532e5048
Barrett Alexander <barrett@clockwork.net>
2016-02-10 15:49:21 -0600

    Updated router

--- router.js
+++ router.js
@@ -2,14 +2,11 @@ var plugins = require(process.cwd() + '/app/services/plugins');
 var Controller = plugins.require('controllers/controller');
 var ExportController = require('./controller');
 
-module.exports = function(event) {
-    var router  = new event.express.Router();
-    var factory = new Controller.Factory(ExportController, event.app);
+module.exports = function(router, app) {
+    var factory = new Controller.Factory(ExportController, app);
 
     router.get('/asset/:name', factory.handle('asset'));
     router.get('/download/:id', factory.handle('download'));
 
-    event.app.io.route('export/compress', factory.handle('compress'));
-
-    return router;
+    app.io.route('export/compress', factory.handle('compress'));
 };
